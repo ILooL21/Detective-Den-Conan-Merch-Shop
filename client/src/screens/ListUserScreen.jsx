b205c896677150d0b3982e926e66ef83d4746e2b
Muhammad Kholilur Rohman <muhammadkholilurrohman72@gmail.com>
2024-03-24 23:13:17 +0700

    menambahkan search pada halaman list user

--- client/src/screens/ListUserScreen.jsx
+++ client/src/screens/ListUserScreen.jsx
@@ -1,11 +1,16 @@
 import { useGetAllUsersQuery, useChangeRoleMutation } from "../slices/usersApiSlice";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Table, Button } from "react-bootstrap";
 import Loader from "../components/Loader";
 import { setCredentials } from "../slices/authSlice";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { FaSearch } from "react-icons/fa";
 
 const ListUserScreen = () => {
+  const [list, setList] = useState([]);
+  const [search, setSearch] = useState("");
+
   const dispatch = useDispatch();
 
   const { data } = useGetAllUsersQuery();
@@ -24,15 +29,52 @@ const ListUserScreen = () => {
     window.location.reload();
   };
 
+  const searchHandler = () => {
+    if (search !== "") {
+      const results = data?.filter((user) => {
+        return user.name.toLowerCase().startsWith(search.toLowerCase());
+      });
+      if (results.length === 0) {
+        toast.error("User not found");
+      } else {
+        setList(results);
+      }
+    } else {
+      setList(data);
+    }
+  };
+
+  useEffect(() => {
+    setList(data);
+  }, [data]);
+
   return (
     <div>
-      <h3>Users</h3>
+      <div className="d-flex justify-content-between align-items-center my-3">
+        <h3 className="">Users</h3>
+        <div className="d-flex align-items-center">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Search by email or name"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            style={{ width: "200px" }} // Adjust the width as needed
+          />
+          <Button
+            className="btn btn-primary ms-2" // Adjust margin as needed
+            onClick={searchHandler}>
+            <FaSearch />
+          </Button>
+        </div>
+      </div>
       <Table
         striped
         bordered
         hover>
         <thead>
           <tr>
+            <th>No.</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
@@ -40,10 +82,11 @@ const ListUserScreen = () => {
           </tr>
         </thead>
         <tbody>
-          {data?.map((user) => (
+          {list?.map((user) => (
             <tr
               key={user._id}
               onSubmit={clickHandler}>
+              <td>{list.indexOf(user) + 1}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>{user.role}</td>
