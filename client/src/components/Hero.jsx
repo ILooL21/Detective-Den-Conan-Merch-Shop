bb1c436fffdffd6b4a8faed0aaff463bedee10c8
Muhammad Kholilur Rohman <muhammadkholilurrohman72@gmail.com>
2024-03-25 08:13:15 +0700

    ganti register ke sign up

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -35,7 +35,7 @@ const Hero = () => {
                   <Button
                     variant="secondary"
                     href="/register">
-                    Register
+                    Sign Up
                   </Button>
                 </>
               )}
