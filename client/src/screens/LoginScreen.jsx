66e8cf95f72993c27726f42f0b44603a0eb8a274
BariqAbrarRamadhan <bariqrama02@gmail.com>
2024-04-05 09:09:37 +0700

    footer styling

--- client/src/screens/LoginScreen.jsx
+++ client/src/screens/LoginScreen.jsx
@@ -1,85 +1,95 @@
-import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import FormContainer from "../components/FormContainer";
-import { useDispatch, useSelector } from "react-redux";
-import { useLoginMutation } from "../slices/usersApiSlice";
-import { setCredentials } from "../slices/authSlice";
-import { toast } from "react-toastify";
-import Loader from "../components/Loader";
+// import { useState, useEffect } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import { Form, Button, Row, Col } from "react-bootstrap";
+// // import FormContainer from "../components/FormContainer";
+// import { useDispatch, useSelector } from "react-redux";
+// import { useLoginMutation } from "../slices/usersApiSlice";
+// import { setCredentials } from "../slices/authSlice";
+// import { toast } from "react-toastify";
+// import Loader from "../components/Loader";
+import "../styles/LoginScreen.css";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
 
-  const [login, { isLoading }] = useLoginMutation();
+  // const [login, { isLoading }] = useLoginMutation();
 
-  const { userInfo } = useSelector((state) => state.auth);
+  // const { userInfo } = useSelector((state) => state.auth);
 
-  useEffect(() => {
-    if (userInfo) {
-      navigate("/");
-    }
-  }, [navigate, userInfo]);
+  // useEffect(() => {
+  //   if (userInfo) {
+  //     navigate("/");
+  //   }
+  // }, [navigate, userInfo]);
 
-  const submitHandler = async (e) => {
-    e.preventDefault();
-    try {
-      const res = await login({ email, password }).unwrap();
-      dispatch(setCredentials({ ...res }));
-      navigate("/");
-    } catch (err) {
-      toast.error(err?.data?.message || err.error);
-    }
-  };
+  // const submitHandler = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const res = await login({ email, password }).unwrap();
+  //     dispatch(setCredentials({ ...res }));
+  //     navigate("/");
+  //   } catch (err) {
+  //     toast.error(err?.data?.message || err.error);
+  //   }
+  // };
 
   return (
-    <FormContainer>
-      <h1>Sign In</h1>
+    <div className="container-login">
+      <img
+        src="./src/assets/conan-gaya-auth.jpeg"
+        alt="Conan"
+        className="image-auth"
+      />
+      <div className="login-auth">
+        <h1>Sign In</h1>
+        <form action="" className="form-login">
+          <input type="text" placeholder="Email or Phone Number" />
+          <input type="password" placeholder="Password" />
+        </form>
+        {/* <Form onSubmit={submitHandler}>
+          <Form.Group className="my-2" controlId="email">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-      <Form onSubmit={submitHandler}>
-        <Form.Group
-          className="my-2"
-          controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}></Form.Control>
-        </Form.Group>
+          <Form.Group className="my-2" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group
-          className="my-2"
-          controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}></Form.Control>
-        </Form.Group>
+          <Button
+            disabled={isLoading}
+            type="submit"
+            variant="primary"
+            className="mt-3"
+          >
+            Sign In
+          </Button>
+        </Form> */}
 
-        <Button
-          disabled={isLoading}
-          type="submit"
-          variant="primary"
-          className="mt-3">
-          Sign In
-        </Button>
-      </Form>
+        {/* {isLoading && <Loader />}
 
-      {isLoading && <Loader />}
-
-      <Row className="py-3">
-        <Col>
-          New Customer? <Link to="/register">Register</Link>
-        </Col>
-      </Row>
-    </FormContainer>
+        <Row className="py-3">
+          <Col>
+            New Customer? <Link to="/register">Register</Link>
+          </Col>
+        </Row> */}
+      </div>
+    </div>
   );
 };
 
