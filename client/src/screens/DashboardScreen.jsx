e5ec49363e13484b60aea84eeeea2985f2e7693f
Muhammad Kholilur Rohman <muhammadkholilurrohman72@gmail.com>
2024-05-12 21:24:48 +0700

    kesalahan dasboard

--- client/src/screens/DashboardScreen.jsx
+++ client/src/screens/DashboardScreen.jsx
@@ -9,69 +9,70 @@ const DasboardScreen = () => {
 
   return (
     <>
-      <div className="container-dashboard">
-        <RefreshToken />
-        <div className="container-dashboard-header">
-          <Breadcrumb
-            className="breadcrumb-dashboard"
-            items={[
-              {
-                title: <a href="/">Home</a>,
-              },
-              {
-                title: "Dashboard",
-              },
-            ]}
-          />
-        </div>
-        <div className="container-dashboard-main">
-          <h2>Welcome {userInfo.name}</h2>
-          <div className="container-content-dashboard">
-            <div
-              style={{
-                display: "flex",
-                padding: "136px 0px 0px",
-              }}>
-              <div className="container-navbar-dashboard">
-                <div className="navbar-dashboard-list">
-                  {userInfo.role === "superadmin" && (
-                    <div>
-                      <a href="/listallusers">
-                        <BarsOutlined className="icon-navbar-dashboard" />
-                        List User
-                      </a>
-                    </div>
-                  )}
-                  <div>
-                    <a href="/listcategories">
-                      <BarsOutlined className="icon-navbar-dashboard" />
-                      List Kategori
-                    </a>
-                  </div>
-                  <div>
-                    <a href="/listproducts">
-                      <BarsOutlined className="icon-navbar-dashboard" />
-                      List Product
-                    </a>
-                  </div>
-                  <div>
-                    <a href="/listarticles">
-                      <BarsOutlined className="icon-navbar-dashboard" />
-                      List Artikel
-                    </a>
-                  </div>
-                  <div>
-                    <a href="/support">
-                      <QuestionOutlined className="icon-navbar-dashboard" />
-                      Support
-                    </a>
-                  </div>
-                  <div className="content-dashboard">
-                    <a href="/listriddles">List Riddle</a>
-                  </div>
-                </div>
+      <div
+        style={{
+          display: "flex",
+          padding: "136px 0px 0px",
+        }}>
+        <div className="container-navbar-dashboard">
+          <div className="navbar-dashboard-list">
+            {userInfo.role === "superadmin" && (
+              <div>
+                <a href="/listallusers">
+                  <BarsOutlined className="icon-navbar-dashboard" />
+                  List User
+                </a>
               </div>
+            )}
+            <div>
+              <a href="/listcategories">
+                <BarsOutlined className="icon-navbar-dashboard" />
+                List Kategori
+              </a>
             </div>
+            <div>
+              <a href="/listproducts">
+                <BarsOutlined className="icon-navbar-dashboard" />
+                List Product
+              </a>
+            </div>
+            <div>
+              <a href="/listarticles">
+                <BarsOutlined className="icon-navbar-dashboard" />
+                List Artikel
+              </a>
+            </div>
+            <div>
+              <a href="/listriddles">
+                <BarsOutlined className="icon-navbar-dashboard" />
+                List Riddle
+              </a>
+            </div>
+            <div>
+              <a href="/support">
+                <QuestionOutlined className="icon-navbar-dashboard" />
+                Support
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="container-dashboard">
+          <RefreshToken />
+          <div className="container-dashboard-header">
+            <Breadcrumb
+              className="breadcrumb-dashboard"
+              items={[
+                {
+                  title: <a href="/">Home</a>,
+                },
+                {
+                  title: "Dashboard",
+                },
+              ]}
+            />
+          </div>
+          <div className="container-dashboard-main">
+            <h2>Welcome {userInfo.name}</h2>
           </div>
         </div>
       </div>
