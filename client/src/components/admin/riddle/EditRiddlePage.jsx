b4f52e4f1480d6fcf2ec38fd498aa369f58f5815
BariqAbrarRamadhan <bariqrama02@gmail.com>
2024-05-15 22:53:34 +0700

    Style edit riddle

--- client/src/components/admin/riddle/EditRiddlePage.jsx
+++ client/src/components/admin/riddle/EditRiddlePage.jsx
@@ -1,8 +1,16 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useGetSingleRiddleQuery, useUpdateRiddleMutation, useAddTersangkaMutation, useDeleteTersangkaMutation } from "../../../slices/riddleApiSlice";
-import { Form, Button } from "react-bootstrap";
+import {
+  useGetSingleRiddleQuery,
+  useUpdateRiddleMutation,
+  useAddTersangkaMutation,
+  useDeleteTersangkaMutation,
+} from "../../../slices/riddleApiSlice";
+import { Form } from "react-bootstrap";
 import { toast } from "react-toastify";
+import { Breadcrumb } from "antd";
+("@ant-design/icons");
+import "../../../styles/riddle/EditRiddlePage.css";
 
 const EditRiddlePage = () => {
   let { id } = useParams();
@@ -68,138 +76,171 @@ const EditRiddlePage = () => {
     }
   }, [riddle]);
 
+  useEffect(() => {
+    const textareas = document.querySelectorAll("textarea");
+    textareas.forEach((textarea) => {
+      textarea.style.height = "auto";
+      textarea.style.height = textarea.scrollHeight + "px";
+    });
+  }, [kronologi]);
+
   return (
-    <div
-      style={{
-        padding: "25vh 100px",
-      }}>
-      <h1>Edit Riddle</h1>
+    <div className="container-edit-riddle">
+      <div className="container-edit-riddle-header">
+        <Breadcrumb
+          className="breadcrumb-edit-riddle"
+          items={[
+            {
+              title: <a href="/">Home</a>,
+            },
+            {
+              title: <a href="/dashboard">Dashboard</a>,
+            },
+            {
+              title: <a href="/listriddles">List Riddle</a>,
+            },
+            {
+              title: "Edit Riddle",
+            },
+          ]}
+        />
+      </div>
       {isLoading && riddle ? (
         <p>Loading...</p>
       ) : (
         <>
-          <Form>
-            <Form.Group controlId="image">
-              <Form.Label>Image</Form.Label>
-              <Form.Control
-                type="file"
-                onChange={(e) => {
-                  setImage(e.target.files[0]);
-                  setPreview(URL.createObjectURL(e.target.files[0]));
-                }}
-              />
-              {preview ? (
-                <img
-                  src={preview}
-                  alt="preview"
-                  style={{
-                    width: "200px",
-                    height: "200px",
+          <Form className="container-form-edit-riddle">
+            <div className="container-form-edit-riddle-hint">
+              <h3>Add Hint</h3>
+              <Form.Group
+                controlId="image"
+                className="container-form-edit-riddle-image"
+              >
+                <Form.Label>Image</Form.Label>
+                <Form.Control
+                  type="file"
+                  onChange={(e) => {
+                    setImage(e.target.files[0]);
+                    setPreview(URL.createObjectURL(e.target.files[0]));
                   }}
                 />
-              ) : (
-                <img
-                  src={`http://localhost:8080/${image}`}
-                  alt="preview"
-                  style={{
-                    width: "200px",
-                    height: "200px",
-                  }}
-                />
-              )}
-            </Form.Group>
-            <Form.Group controlId="title">
-              <Form.Label>Title</Form.Label>
-              <Form.Control
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="kronologi">
-              <Form.Label>Kronologi</Form.Label>
-              <Form.Control
-                as="textarea"
-                value={kronologi}
-                onChange={(e) => setKronologi(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="clue">
-              <Form.Label>Clue</Form.Label>
-              <Form.Control
-                type="text"
-                value={clue}
-                onChange={(e) => setClue(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="answer">
-              <Form.Label>Pelaku</Form.Label>
-              <Form.Control
-                as="select"
-                value={pelaku}
-                onChange={(e) => setPelaku(e.target.value)}>
-                {tersangka?.length > 0 ? (
-                  tersangka?.map((tersangka, index) => (
-                    <option
-                      key={index}
-                      value={tersangka.tersangka}>
-                      {tersangka.tersangka}
-                    </option>
-                  ))
+                {preview ? (
+                  <img src={preview} alt="preview" />
                 ) : (
-                  <option value="">No Tersangka</option>
+                  <img src={`http://localhost:8080/${image}`} alt="preview" />
                 )}
-              </Form.Control>
-              <Form.Label>alasan</Form.Label>
+              </Form.Group>
+              <Form.Group
+                controlId="title"
+                className="container-form-edit-riddle-title"
+              >
+                <Form.Label>Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group
+                controlId="kronologi"
+                className="container-form-edit-riddle-kronologi"
+              >
+                <Form.Label>Kronologi</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  value={kronologi}
+                  onChange={(e) => setKronologi(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group
+                controlId="clue"
+                className="container-form-edit-riddle-clue"
+              >
+                <Form.Label>Clue</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={clue}
+                  onChange={(e) => setClue(e.target.value)}
+                />
+              </Form.Group>
+            </div>
+            <div className="container-form-edit-riddle-add-tersangka">
+              <h3>Add Tersangka</h3>
+              <Form.Group
+                controlId="tersangka"
+                className="container-form-edit-riddle-tersangka"
+              >
+                <Form.Label>Nama Tersangka</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={(e) => setNama(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group
+                controlId="alibi"
+                className="container-form-edit-riddle-alibi"
+              >
+                <Form.Label>Alibi</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  onChange={(e) => setAlibi(e.target.value)}
+                />
+              </Form.Group>
+              <div className="container-button-add-tersangka">
+                <button onClick={handleAddTersangka}>Add Tersangka</button>
+              </div>
+              <Form.Label>List Tersangka</Form.Label>
+
+              {tersangka?.length > 0 ? (
+                tersangka.map((tersangka, index) => (
+                  <div className="container-list-tersangka" key={index}>
+                    <span></span>
+                    <h5>Tersangka {index + 1}</h5>
+                    <p>
+                      <b>{tersangka.tersangka}</b>
+                    </p>
+                    <p>Alibi: {tersangka.alibi}</p>
+                    <div className="container-button-edit-riddle">
+                      <button
+                        className="button-submit-edit-riddle"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setPelaku(tersangka.tersangka);
+                        }}
+                      >
+                        Pilih Tersangka
+                      </button>
+                      <button
+                        className="button-delete-edit-riddle"
+                        onClick={() =>
+                          handleDeleteTersangka(tersangka.tersangka)
+                        }
+                      >
+                        Hapus Tersangka
+                      </button>
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <p>Tidak ada Tersangka</p>
+              )}
+            </div>
+            <Form.Group
+              controlId="answer"
+              className="container-form-edit-riddle-pelaku"
+            >
+              <Form.Label>Pelaku</Form.Label>
+              <Form.Control type="text" value={pelaku} disabled></Form.Control>
+              <Form.Label>Alasan</Form.Label>
               <Form.Control
                 as="textarea"
                 value={alasan !== null ? alasan : ""}
                 onChange={(e) => setAlasan(e.target.value)}
               />
             </Form.Group>
-
-            <Button
-              variant="primary"
-              onClick={handleSave}>
-              Save
-            </Button>
-          </Form>
-          <Form>
-            <Form.Label>List Tersangka</Form.Label>
-            {tersangka?.length > 0 ? (
-              tersangka.map((tersangka, index) => (
-                <ul key={index}>
-                  <li>{tersangka.tersangka}</li>
-                  <p>{tersangka.alibi}</p>
-                  <Button
-                    variant="danger"
-                    onClick={() => handleDeleteTersangka(tersangka.tersangka)}>
-                    Delete
-                  </Button>
-                </ul>
-              ))
-            ) : (
-              <p>No Tersangka</p>
-            )}
-            <Form.Group controlId="tersangka">
-              <Form.Label>Nama Tersangka</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={(e) => setNama(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="alibi">
-              <Form.Label>Alibi</Form.Label>
-              <Form.Control
-                as="textarea"
-                onChange={(e) => setAlibi(e.target.value)}
-              />
-            </Form.Group>
-            <Button
-              variant="primary"
-              onClick={handleAddTersangka}>
-              Add Tersangka
-            </Button>
+            <div className="container-button-save-riddle">
+              <button onClick={handleSave}>Save Riddle</button>
+            </div>
           </Form>
         </>
       )}
