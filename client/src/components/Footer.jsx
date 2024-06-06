f5eb103911e8a4fb67d4638418d5f344dafc32fc
Muhammad Kholilur Rohman <muhammadkholilurrohman72@gmail.com>
2024-06-06 16:25:44 +0700

    final?

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,8 +16,7 @@ const FooterComponent = () => {
           justifyContent: "center",
           height: "300px",
           backgroundColor: "#000",
-        }}
-      >
+        }}>
         <div className="footer-container">
           <div className="footer-links">
             <h1>Exclusive</h1>
@@ -25,12 +24,21 @@ const FooterComponent = () => {
           </div>
           <div className="footer-links">
             <h1>Support</h1>
-            <a target="blank" href="https://www.google.com/maps/place/Politeknik+Elektronika+Negeri+Surabaya+(PENS)+PSDKU+Lamongan/@-7.1160776,112.4286083,18z/data=!4m6!3m5!1s0x2e77fb5639db7bbf:0x38f9dbc7075722fa!8m2!3d-7.1155235!4d112.4278539!16s%2Fg%2F11nnphycp8?entry=ttu">
-              Jl. Panglima Sudirman No.125, Sawah, Banjarmendalan, Kec.
-              Lamongan, Kabupaten Lamongan, Jawa Timur 62212
+            <a
+              target="blank"
+              href="https://www.google.com/maps/place/Politeknik+Elektronika+Negeri+Surabaya+(PENS)+PSDKU+Lamongan/@-7.1160776,112.4286083,18z/data=!4m6!3m5!1s0x2e77fb5639db7bbf:0x38f9dbc7075722fa!8m2!3d-7.1155235!4d112.4278539!16s%2Fg%2F11nnphycp8?entry=ttu">
+              Jl. Panglima Sudirman No.125, Sawah, Banjarmendalan, Kec. Lamongan, Kabupaten Lamongan, Jawa Timur 62212
             </a>
-            <a target="blank" href="https://pens.ac.id">exclusive@gmail.com</a>
-            <a target="blank" href="https://wa.me/6285745820511">+6285745820511</a>
+            <a
+              target="blank"
+              href="https://wa.me/6285745820511">
+              +6285745820511
+            </a>
+            <a
+              target="blank"
+              href="https://pens.ac.id">
+              muhammadkholilurrohman72@gmail.com
+            </a>
           </div>
           <div className="footer-links">
             <h1>Account</h1>
@@ -64,8 +72,7 @@ const FooterComponent = () => {
           height: "60px",
           backgroundColor: "#000",
           borderTop: "1px solid #444",
-        }}
-      >
+        }}>
         <div className="footer-cr">
           <a>© Copyright 2024. All right reserved</a>
         </div>
--- client/src/screens/ListMyOrderScreen.jsx
+++ client/src/screens/ListMyOrderScreen.jsx
@@ -1,8 +1,4 @@
-import {
-  useGetMyOrdersQuery,
-  useCancelOrderMutation,
-  useSelesaiOrderMutation,
-} from "../slices/orderApiSlice";
+import { useGetMyOrdersQuery, useCancelOrderMutation, useSelesaiOrderMutation } from "../slices/orderApiSlice";
 import { useReviewsProductMutation } from "../slices/productApiSlice";
 import { Card } from "react-bootstrap";
 import { Rate, Breadcrumb } from "antd";
@@ -50,133 +46,117 @@ const ListMyOrderScreen = () => {
   return (
     <div className="container-list-order">
       <div>
-          <div className="d-flex justify-content-between align-items-center my-3">
-            <div className="container-list-order-header">
-              <Breadcrumb
-                className="breadcrumb-list-order"
-                items={[
-                  {
-                    title: <a href="/">Home</a>,
-                  },
-                  {
-                    title: <a href="/dashboard">Dashboard</a>,
-                  },
-                  {
-                    title: "List Order",
-                  },
-                ]}
-              />
-            </div>
+        <div className="d-flex justify-content-between align-items-center my-3">
+          <div className="container-list-order-header">
+            <Breadcrumb
+              className="breadcrumb-list-order"
+              items={[
+                {
+                  title: <a href="/">Home</a>,
+                },
+                {
+                  title: <a href="/profile">Profile</a>,
+                },
+                {
+                  title: "List Order",
+                },
+              ]}
+            />
           </div>
+        </div>
       </div>
       {isLoading ? (
         <h2>Loading...</h2>
       ) : (
         <div>
           {orders.map((order) => (
-            <Card key={order._id} style={{marginBottom: "24px", border: "solid 1px #db4444"}}>
-            <Card.Body>
-              <b>Order ID</b>
-              <Card.Text>{order._id}</Card.Text>
-              <b>List Order</b>
-                  <table className="table-list-order">
-                    <thead>
-                      <tr>
-                        <th>Product</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Subtotal</th>
-                        {order.status === "Selesai" && <th>Rating</th>}
+            <Card
+              key={order._id}
+              style={{ marginBottom: "24px", border: "solid 1px #db4444" }}>
+              <Card.Body>
+                <b>Order ID</b>
+                <Card.Text>{order._id}</Card.Text>
+                <b>List Order</b>
+                <table className="table-list-order">
+                  <thead>
+                    <tr>
+                      <th>Product</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th>Subtotal</th>
+                      {order.status === "Selesai" && <th>Rating</th>}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {order.orderItems.map((item) => (
+                      <tr key={item._id}>
+                        <td>{item.product}</td>
+                        <td>{item.price}</td>
+                        <td>{item.quantity}</td>
+                        <td>{item.price * item.quantity}</td>
+                        {order.status === "Selesai" ? (
+                          item.rating ? (
+                            <td>
+                              <Rate
+                                className="rating-icon-product"
+                                value={item.rating}
+                                onChange={(value) => handleRating(item.id, value)}
+                              />
+                            </td>
+                          ) : (
+                            <td>
+                              <Rate
+                                className="rating-icon-product"
+                                value={0}
+                                onChange={(value) => handleRating(item.id, value)}
+                              />
+                            </td>
+                          )
+                        ) : null}
                       </tr>
-                    </thead>
-                    <tbody>
-                      {order.orderItems.map((item) => (
-                        <tr key={item._id}>
-                          <td>{item.product}</td>
-                          <td>{item.price}</td>
-                          <td>{item.quantity}</td>
-                          <td>{item.price * item.quantity}</td>
-                          {order.status === "Selesai" ? (
-                            item.rating ? (
-                              <td>
-                                <Rate
-                                  className="rating-icon-product"
-                                  value={item.rating}
-                                  onChange={(value) =>
-                                    handleRating(item.id, value)
-                                  }
-                                />
-                              </td>
-                            ) : (
-                              <td>
-                                <Rate
-                                  className="rating-icon-product"
-                                  value={0}
-                                  onChange={(value) =>
-                                    handleRating(item.id, value)
-                                  }
-                                />
-                              </td>
-                            )
-                          ) : null}
-                        </tr>
-                      ))}
-                      <tr>
-                          <td colSpan={3}><b>Total</b></td>
-                          <td colSpan={2}>{order.totalPrice}</td>
-                        </tr>
-                    </tbody>
-                  </table>
-                  <b>Nomor Resi</b>
-                <Card.Text>
-                  {order.resi ? order.resi : "Belum ada resi"}
-                </Card.Text>
-                  <b>Alamat</b>
+                    ))}
+                    <tr>
+                      <td colSpan={3}>
+                        <b>Total</b>
+                      </td>
+                      <td colSpan={2}>{order.totalPrice}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <b>Nomor Resi</b>
+                <Card.Text>{order.resi ? order.resi : "Belum ada resi"}</Card.Text>
+                <b>Alamat</b>
                 <Card.Text>{order.shippingAddress}</Card.Text>
-                  <b>Status</b>
+                <b>Status</b>
                 <Card.Text>{order.status}</Card.Text>
-                  <b>Tanggal Pesanan</b>
-                <Card.Text>
-                  {new Date(order.createdAt).toLocaleString("id-ID")}
-                </Card.Text>
+                <b>Tanggal Pesanan</b>
+                <Card.Text>{new Date(order.createdAt).toLocaleString("id-ID")}</Card.Text>
                 {order.status === "Belum Dibayar" && (
                   <Card.Text>
-                    Hubungi
-                    <a
-                      href={`https://wa.me/6285745820511?text=Halo%20saya%20ingin%20membayar%20pesanan%20dengan%20id%20${order._id}`}
-                    >
-                      085745820511
-                    </a>
-                    untuk melakukan pembayaran, admin akan mengonfirmasi
-                    pembayaran agar barang segera di proses
+                    Hubungi <a href={`https://wa.me/6285745820511?text=Halo%20saya%20ingin%20membayar%20pesanan%20dengan%20id%20${order._id}`}>085745820511</a> untuk melakukan pembayaran, admin akan mengonfirmasi pembayaran agar barang
+                    segera di proses
                   </Card.Text>
                 )}
                 {order.status === "Diproses" && (
                   <Card.Text>
-                    Barang sedang diproses, nomor resi akan tertera disini saat
-                    barang sudah dikirim. jika ingin melakukan pembatalan segera
-                    konfirmasi ke
-                    <a
-                      href={`https://wa.me/6285745820511?text=Halo%20saya%20ingin%20membatalkan%20pesanan%20dengan%20id%20${order._id}`}
-                    >
-                      085745820511
-                    </a>
-                    (admin)
+                    Barang sedang diproses, nomor resi akan tertera disini saat barang sudah dikirim. jika ingin melakukan pembatalan segera konfirmasi ke{" "}
+                    <a href={`https://wa.me/6285745820511?text=Halo%20saya%20ingin%20membatalkan%20pesanan%20dengan%20id%20${order._id}`}> 085745820511 </a> (admin)
                   </Card.Text>
                 )}
               </Card.Body>
-              {(order.status === "Dikirim" ||
-                (order.status !== "Dibatalkan" &&
-                  order.status !== "Selesai")) && (
+              {(order.status === "Dikirim" || (order.status !== "Dibatalkan" && order.status !== "Selesai")) && (
                 <Card.Footer>
                   {order.status === "Dikirim" && (
-                    <button className="button-order-green" onClick={() => handleSelesaiOrder(order._id)}>
+                    <button
+                      className="button-order-green"
+                      onClick={() => handleSelesaiOrder(order._id)}>
                       Selesai
                     </button>
                   )}
-                  {(order.status === "Belum Dibayar" ||
-                    order.status === "Diproses") && (
-                    <button className="button-order" onClick={() => handleCancelOrder(order._id)}>
+                  {(order.status === "Belum Dibayar" || order.status === "Diproses") && (
+                    <button
+                      className="button-order"
+                      onClick={() => handleCancelOrder(order._id)}>
                       Batalkan
                     </button>
                   )}
